f6fee14500f6d5a3ae10cab94440c7c46bbd8822
Travis Herrick <quasimonomial@gmail.com>
2014-12-17 00:20:28 -0800

    prices comparison done

--- app/models/price.rb
+++ app/models/price.rb
@@ -12,10 +12,16 @@
 #
 
 class Price < ActiveRecord::Base
+  validates_uniqueness_of :product_id, scope: [:pricer_id, :pricer_type]
+
 	belongs_to :pricer, :polymorphic => true
 	belongs_to :product
 
-	def in_price_range?(user)
-
+	def ouside_price_range?(user)
+    user_price_obj = user.prices.where(product_id: self.product_id).first
+    return false unless user_price_obj
+    user_price = user_price_obj.price
+    return user_price
+    ((user_price - self.price)/user_price * 100) >= user.price_range_percentage
   end
 end
