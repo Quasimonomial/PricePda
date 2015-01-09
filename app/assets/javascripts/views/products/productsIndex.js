27bb2bbc42e63379989f2d190c929e2884744ece
Travis Herrick <quasimonomial@gmail.com>
2015-01-09 00:37:58 -0600

    hit a wall with naming variables in products, realzied coding is impossible

--- app/assets/javascripts/views/products/productsIndex.js
+++ app/assets/javascripts/views/products/productsIndex.js
@@ -6,55 +6,49 @@ Vetpda.Views.ProductsIndex = Backbone.View.extend({
 		this.listenTo(this.collection, 'sync add remove', this.render)
 	},
 
-	// events: {
-	// 	'click .companyDelete': 'destroyCompany',
-	// 	'submit form' : 'addCompany'
-	// },
+	events: {
+		'click .productDelete': 'destroyProduct',
+		'submit form' : 'addProduct'
+	},
 
-	// addCompany: function(event){
-	// 	event.preventDefault();
+	addProduct: function(event){
+		event.preventDefault();
 
-	// 	var company = new Vetpda.Models.Company
-		
-	// 	console.log('adding a company');
-	// 	console.log($(event.target))
-	// 	var attrs = $(event.target).serializeJSON();
+		var product = new Vetpda.Models.Product
+
+		var attrs = $(event.target).serializeJSON();
 		
 
-	// 	var success = function () {
-	//       console.log("win");
-	//       this.collection.add(company, { merge: true });
-	//       //Backbone.history.navigate("", { trigger: true });
-	//     }.bind(this)
+		var success = function () {
+	      this.collection.add(product, { merge: true });
+	    }.bind(this)
 
-	//     function errors (model, response) {
-	//       console.log("fail");
-	//       $('.errors').empty();
-	//       response.responseJSON.forEach(function (el) {
-	//         var li = $('<li></li>');
-	//         li.html(el);
-	//         $('.errors').append(li);
-	//       }.bind(this));
-	//     }
+	    function errors (model, response) {
+	      $('.errors').empty();
+	      response.responseJSON.forEach(function (el) {
+	        var li = $('<li></li>');
+	        li.html(el);
+	        $('.errors').append(li);
+	      }.bind(this));
+	    }
 
-	//     company.save(attrs, {
-	//       success: success,
-	//       error: errors.bind(this)
-	//     });
-	// },
+	    product.save(attrs, {
+	      success: success,
+	      error: errors.bind(this)
+	    });
+	},
 
-	// destroyCompany: function(event){
-	// 	event.preventDefault();
+	destroyProduct: function(event){
+		event.preventDefault();
 
-	// 	var $target = $(event.currentTarget);
+		var $target = $(event.currentTarget);
 
-	// 	console.log($target);
-	// 	console.log($target.attr('data-id'));
+		console.log($target);
+		console.log($target.attr('data-id'));
 
-	// 	var company = this.collection.get($target.attr('data-id'));
-	// 	company.destroy();
-	// 	console.log("Destroy");
-	// },
+		var product = this.collection.get($target.attr('data-id'));
+		product.destroy();
+	},
 
 	render: function(){
 		var content = this.template({
