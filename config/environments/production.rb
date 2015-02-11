2614b7895168b2618788632aed932ca8f850e345
Travis Herrick <quasimonomial@gmail.com>
2015-02-11 14:15:27 -0800

    Another typo I think

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.stmp_settings ={
+  config.action_mailer.stmp_settings = {
     adress: 'stmp.sendgrid.net',
     port: '587',
     authentication: :plain,
