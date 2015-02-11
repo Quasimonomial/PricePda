78c4a07b762de5ce16d132c1c13eec33ea37a9c3
Travis Herrick <quasimonomial@gmail.com>
2015-02-11 15:30:43 -0800

    Uploading email stuff, with config settings commented out

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,15 +60,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.stmp_settings = {
-    # adress: 'stmp.sendgrid.net',
-    # port: '587',
-    # authentication: :plain,
-    # user_name: ENV['app33882497@heroku.com'],
-    # password: ENV['z9o344l1'],
-    # domain: 'heroku.com',
-    # enable_starttls_auto: true 
-  }
+  # config.action_mailer.stmp_settings = {
+  #   # adress: 'stmp.sendgrid.net',
+  #   # port: '587',
+  #   # authentication: :plain,
+  #   # user_name: ENV['app33882497@heroku.com'],
+  #   # password: ENV['z9o344l1'],
+  #   # domain: 'heroku.com',
+  #   # enable_starttls_auto: true 
+  # }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
