6c406bbc50a30e07ddaea1644bd3a95ec3cc0cc2
Travis Herrick <quasimonomial@gmail.com>
2015-02-17 12:30:58 -0800

    deleted some unesseasrey code

--- app/assets/javascripts/models/product.js
+++ app/assets/javascripts/models/product.js
@@ -2,9 +2,7 @@ Vetpda.Models.Product = Backbone.Model.extend({
 	urlRoot: "/api/products", 
 	initialize: function () {
 	    Backbone.Model.prototype.initialize.apply(this, arguments);
-	    // this.on("change", function (model, options) {
-	    // if (options && options.save === false) return;
-	    //   model.save();
-	    // });
+
 	 }
+	 
 });
