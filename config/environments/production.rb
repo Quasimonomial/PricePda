cae2da4706f622f1f8f9354a50f9c9b1a943e4c5
Travis Herrick <quasimonomial@gmail.com>
2015-02-18 15:10:57 -0800

    more testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-      config.action_mailer.default_url_options = { :host => 'vetpdav1' }
+      config.action_mailer.default_url_options = { :host => 'vetpdav1.heroku.com' }
   config.action_mailer.smtp_settings = {
     adress: 'stmp.sendgrid.net',
     port: '587',
