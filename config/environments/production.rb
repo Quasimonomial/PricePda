fe9aaf8947a539410ab20e14506a6bdd88b9e7f4
Travis Herrick <quasimonomial@gmail.com>
2015-02-18 15:37:56 -0800

    more config stuff for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,17 +60,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
-  config.action_mailer.smtp_settings = {
-    adress: 'stmp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true 
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => 'heroku.com'
   }
-
+  config.action_mailer.default_url_options = { :host => 'vetpdav1.heroku.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
