4f0438abe66d7e49642c432d41ff8080b77e7f5a
Travis Herrick <quasimonomial@gmail.com>
2015-02-19 16:27:00 -0800

    Deleted unessesary jsonify file, cleaned up routes, changed splash on root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :products
-    resources :companies
-    resource :user_percents#, only: [:show, :update]
+    resources :products, except: [:edit, :new]
+    resources :companies, except: [:edit, :new]
+    resource :user_percents, only: [:show, :create]
   end 
 
-  resources :users
-  resource :session
+  resources :users, only: [:update, :create, :edit, :new]
+  resource :session, only: [:create, :new, :destroy]
 
 end
