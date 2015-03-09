0f8a53f719f9607cc7cac76522a4373ae53861d7
Travis Herrick <quasimonomial@gmail.com>
2015-03-08 23:57:40 -0700

    cleaned up user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -76,17 +76,15 @@ class User < ActiveRecord::Base
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  def valid_password?(password)
-    BCrypt::Password.new(self.password_digest).is_password?(password)
-  end
-
   def reset_token!
     self.session_token = SecureRandom.urlsafe_base64(16)
     self.save!
     self.session_token
   end
 
-
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
   private
   
